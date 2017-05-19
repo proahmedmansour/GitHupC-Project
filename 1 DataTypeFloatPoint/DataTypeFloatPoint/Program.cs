783428d040ac1f6f368ed3feea1eb858f0de295c
Pro-Ahmed Mansour <ahmed.mohamed.mansour1996@gmail.com>
2017-05-18 19:57:24 -1100

    DataTypes (float , double , decimal, char , string , Bool)
    This Program Is Explanning This Types In C#

---
+++ 1 DataTypeFloatPoint/DataTypeFloatPoint/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypeFloatPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            //float Point Types
+            float flMaxVar = float.MaxValue;
+            float flMinVal = float.MinValue;
+            Console.WriteLine("Max Value = {0}\nMin Value = {1}",flMaxVar,flMinVal);
+
+            float flVar = 1F / 3;
+            double doVar = 1D / 3;
+            decimal deVar = 1M / 3;
+
+            Console.WriteLine("\nFloat Var = {0} \nDouble Var = {1} \nDecimal Var = {2}", flVar, doVar, deVar); 
+
+            /*********************************************************************************************************/
+            //Text Types
+
+            int charMin = char.MinValue;
+            int charMax = char.MaxValue;
+            
+            char var = 'G';
+            int myVarValue = 'G';
+            char myVarValue2 = Convert.ToChar(myVarValue);
+
+            Console.WriteLine("Min Char = {0} , Max Char = {1} , My Charachter = {2} My Char Number(Ascii Code) = {3} Converted Char = {4}",charMin , charMax, var , myVarValue ,myVarValue2);
+
+            string stringVar = "This is string Variable";
+
+            Console.WriteLine("The String Variable Is = {0}", stringVar);
+
+            bool boolVar = true;
+            Console.WriteLine("Your Boolean Variable = {0}", boolVar);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2 Challange1/Challange1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challange1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            /* Define a var that can accept the 'A' value and print that number value in console screen */
+            char varA = 'A';
+            int charNumberAValue = varA;
+            Console.WriteLine("{0} = {1}" , varA ,charNumberAValue);
+
+            /* Define a var that can accept the 'a' value and print that number value in console screen */
+            char vara = 'a';
+            int charNumberaValue = vara;
+            Console.WriteLine("{0} = {1}", vara, charNumberaValue);
+
+            /*Define a sring var and assigned that value "This is my character A value"*/
+            string stringVarA = "This is my character A value";
+            Console.WriteLine("{0}", stringVarA);
+
+            /*Define a sring var and assigned that value "This is my character a value"*/
+            string stringVara = "This is my character a value";
+            Console.WriteLine("{0}", stringVara);
+
+            /* print the variables in the form "This is my character A varaible # and This is my character a varaible # " */
+            Console.WriteLine("{0} {1} {2} {3}", stringVarA ,charNumberAValue ,stringVara ,charNumberaValue);
+              
+            Console.ReadKey();
+        }
+    }
+}
